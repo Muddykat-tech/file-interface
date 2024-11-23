e6d409eaee3844c01ebadd4688dc749707869e0d
muddykat <jarrod.muddyman@muddykat.tech>
2024-11-23 21:43:42 +1100

    Updated Package Dependencies

--- source/types.ts
+++ source/types.ts
@@ -1,6 +1,6 @@
 import type { GoogleDriveClient } from "@buttercup/googledrive-client";
 import type { WebDAVClient } from "webdav";
-import { ButtercupServerClient } from "@amalsaju/buttercup-server-client";
+import { ButtercupServerClient } from "@muddykat-tech/buttercup-server-client";
 
 export interface DropboxInterfaceConfig {
     dropboxClient: any;
